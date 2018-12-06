76e354758f03c67a313c208ab0625b488640ffb7
Fadi NOUFAL <noufal4me@gmail.Com>
2018-12-06 11:48:12 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    LoginComponent,
+    UserComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/model/user.ts
@@ -0,0 +1,15 @@
+import DateTimeFormat = Intl.DateTimeFormat;
+
+
+export class User {
+
+  id: String;
+
+  username: String;
+
+  password: String;
+
+  bio: String;
+
+  createdDate: DateTimeFormat;
+}
