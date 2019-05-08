e37ec89fcc85029426ed2d17c56650a62e9a447b
Fadi NOUFAL <noufal4me@gmail.com>
2019-05-08 21:12:06 +0200

    Admin dashboard

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {PostCreationComponent} from '../post-creation/post-creation.component';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-main-nav',
@@ -12,16 +13,32 @@ import {PostCreationComponent} from '../post-creation/post-creation.component';
   encapsulation: ViewEncapsulation.None,
 })
 export class MainNavComponent {
-
+  TOKEN_KEY = 'AuthToken';
+  loggedIn: boolean;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait])
     .pipe(
       map(result => result.matches)
     );
+  authService: AuthService;
 
   constructor(public _router: Router,
               private breakpointObserver: BreakpointObserver) {}
               ngOnInit() {
+              this.getToken();
     this.isHandset$.subscribe(isHandset => console.log(isHandset));
   }
+  public getToken() {
+    if(sessionStorage.getItem(this.TOKEN_KEY)){
+      this.loggedIn = true;
+    }else{
+    this.loggedIn =  false;
+    }
+  }
 
+  logout() {
+    this.authService.logout();
+    this._router.navigateByUrl('/login');
+    //this.sidebarService.toggle();
+    this.ngOnInit();
+  }
 }
