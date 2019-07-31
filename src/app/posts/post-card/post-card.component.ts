9b3143288c18d824546bffceaa593b93c9e1755b
Fadi NOUFAL <noufal4me@gmail.com>
2019-07-31 20:38:54 +0200

    Add user photo to post card

--- src/app/posts/post-card/post-card.component.ts
+++ src/app/posts/post-card/post-card.component.ts
@@ -3,6 +3,7 @@ import { Post } from '../../models/post.model';
 import { PostService } from '../post-creation/post.service';
 import { PostContent } from 'src/app/models/PostContent.model';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-post-card',
@@ -14,17 +15,38 @@ export class PostCardComponent implements OnInit {
   @Input() post: Post;
   postContent: PostContent;
   starCount: number;
-
+  imageToShow;
+  image;
+  
   constructor(private postService: PostService,
-    public _router: Router) {
+    public _router: Router, private userService: UserService) {
   }
 
   ngOnInit() {
     this.postContent = this.post.postContent[0];
     this.starCount = this.post.starCount;
+    this.getImageByUserId(this.post.username);
   }
 
   goTodetails() {
     this._router.navigate(['posts', this.post.id]);
+  }
+
+  getImageByUserId(name: string) {
+    this.userService.getUserPhoto(name)
+      .subscribe(blob => {
+        this.createImageFromBlob(blob);
+        this.image = blob;
+      });
+  }
+
+  createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      this.imageToShow = reader.result;
+    }, false);
+    if (image) {
+      reader.readAsDataURL(image);
+    }
   }
 }
